4e498917742bbdfdc11523f43b964515278167ab
Elijah Bobzom <ebobzom@gmail.com>
2019-10-30 09:38:44 +0100

    bugfix: fix title and import error

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,6 +1,7 @@
+import { ADD_USER } from '../actions/action';
 export const reducer = (state = {users: []}, action) => {
     switch(action.type){
-        case 'ADD_USER':
+        case ADD_USER:
             return {
                 ...state,
                 users: [...state.users, action.user]
